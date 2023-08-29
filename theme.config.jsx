370b179cbe296156884dbc0dc3e486241f6177b0
calebpierce <caleb@firefli.com>
2023-08-29 13:15:39 -0400

    Create basic structure and add a few doc pages

---
+++ theme.config.jsx
@@ -0,0 +1,17 @@
+export default {
+  logo: <span>usefulHooks</span>,
+  project: {
+    link: 'https://github.com/ridgehkr/hooks/',
+  },
+  footer: {
+    text: (
+      <span>
+        MIT {new Date().getFullYear()} ©{' '}
+        <a href='https://calebpierce.dev/' target='_blank'>
+          Caleb Pierce
+        </a>
+        .
+      </span>
+    ),
+  },
+}
